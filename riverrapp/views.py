1741d129a081c72a925ceb4493cbac010c2a0ac1
Romane <romane71193@gmail.com>
2018-10-12 12:52:37 -0400

    add gig create form

--- riverrapp/views.py
+++ riverrapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Gig
+from .forms import GigForm
 
 # Create your views here.
 @login_required
@@ -22,8 +23,16 @@ def gig_detail(request, id):
 
     return render(request,'gig_detail.html',{"gig":gig})
 
+@login_required(login_url="/")
 def create_gig(request):
-    return render(request,'create_gig.html',{})
+    if request.method == 'POST':
+        gig_form = GigForm(request.POST,request.FILES)
+        print(gig_form.is_valid())
+        print(gig_form.errors())
+    gig_form = GigForm()
+    return render(request,'create_gig.html',{"gig_form": gig_form})
 
+@login_required(login_url="/")
 def my_gigs(request):
+    gigs = Gig.objects.filter(user = request.user)
     return render(request,'my_gigs.html',{})
