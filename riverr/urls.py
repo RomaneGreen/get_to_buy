b45638d4e1559e0855c3c14a8b35c3a6c8a3b097
Romane <romane71193@gmail.com>
2018-10-14 14:21:18 -0400

    add user profile page

--- riverr/urls.py
+++ riverr/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^gigs/(?P<id>[0-9]+)$', views.gig_detail, name='gig_detail'),
     url(r'^create_gig/$', views.create_gig, name='create_gig'),
+    url(r'^profile/(?P<username>\w+)$', views.profile, name='profile'),
     url(r'^my_gigs/$', views.my_gigs, name='my_gigs'),
     url(r'^edit_gig/(?P<id>[0-9]+)$', views.edit_gig, name='edit_gig'),
     url(r'^login/$', views.login, {'template_name': 'templates/login.html'}, name='login'),
